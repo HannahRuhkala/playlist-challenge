19c1d1276370cf49d52a5ef89f52f5b6ea4dec7c
HannahRuhkala <hannahruhkala@gmail.com>
2026-09-22 10:17:01 -0500

    Update playlist.py
    added a new song and artist (I ain't worried, one republic) to the playlist

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("I Ain't Worried", "One Republic"),
 ]
 
 def print_playlist(songs):
